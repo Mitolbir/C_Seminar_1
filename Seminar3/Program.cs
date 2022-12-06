4d73e6c668a67cdfadfd33d9a6d8ab61b2c91309
Dmitry <mitolbir@gmail.com>
2022-12-06 10:30:27 +0300

    Quadrant programm added

---
+++ Seminar3/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Напишите программу, которая принимает на вход 
+координаты точки (X и Y), причём X ≠ 0 и Y ≠ 0 
+и выдаёт номер четверти плоскости, 
+в которой находится эта точка.
+*/
+
+int FindQuadrant(double x, double y)
+{
+    if(x > 0 && y > 0) return 1;
+    if(x < 0 && y > 0) return 2;
+    if(x < 0 && y < 0) return 3;
+    if(x > 0 && y < 0) return 4;
+    return 0;
+}
+
+Console.Write("Input an x-coordinate: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Input an y-coordinate: ");
+double y = Convert.ToDouble(Console.ReadLine());
+
+int quadNum = FindQuadrant(x, y);
+
+Console.WriteLine($"The point A({x}, {y}) is on the {quadNum} quadrant");
