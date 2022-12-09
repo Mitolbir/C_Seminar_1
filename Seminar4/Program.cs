54d1945ecbe92508d165ae3b6fd5825a72fca385
Dmitry <mitolbir@gmail.com>
2022-12-09 10:23:17 +0300

    Num programm added

---
+++ Seminar4/Program.cs
@@ -0,0 +1,22 @@
+﻿/* Напишите программу, которая принимает 
+на вход число и выдаёт количество цифр в числе. */
+
+int NumOfDigits(int num)
+{
+    int count = 0;
+
+    while(num != 0)
+    {
+        count++;
+        num /= 10;
+    }
+    return count;
+}
+
+Console.Write("Input a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = NumOfDigits(number);
+
+Console.WriteLine($"Number of digits in {number} is {result}");
+
